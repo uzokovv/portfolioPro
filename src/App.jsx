a857e8976c2b2fc270194b9d5d75be3cb61a387e
uktamm <uktamuzoqov2@gmail.com>
2025-03-12 22:31:44 +0500

    tuzatish

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import 'animate.css';
-import AboutMe from './components/aboutMe';
-import Skils from './components/skils';
-import Contact from './components/contact';
+import AboutMe from './components/AboutMe';
+import Skils from './components/Skils';
+import Contact from './components/Contact';
 import Projects from './components/Projects';
 
 const App = () => {
